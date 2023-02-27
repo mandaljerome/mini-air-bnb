e12b6ea85eb0c391addf39ff3b415c894842f720
Jerome Mandal <mandaljerome@yahoo.com>
2023-02-27 22:36:55 +0800

    Small Project Done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,28 @@
 import './Card.scss'
-import image from '../assets/image12.png'
 
-const Card = () => {
-  return (
-   <div className="card">
-      <div className="tag">SOLD OUT</div>
-      <img src={image} alt="image12" />
-      <div className="rating">
-         <i className="fa-solid fa-star"></i>
-         <p className="score">5.0</p>
-         <p className="extra-details">(6) • USA</p>
+const Card = (props) => {
+   const PATH = 'src/assets/'
+
+   return (
+      <div className='card'>
+         {props.openSpots != 'null' && (
+            <div className='tag'>{props.availability}</div>
+         )}
+         <img src={PATH + props.coverImg} alt={props.coverImg} />
+         <div className='rating'>
+            <i className='fa-solid fa-star'></i>
+            <p className='score'>{props.rating}</p>
+            <p className='extra-details'>
+               ({props.reviewCount}) • {props.location}
+            </p>
+         </div>
+         <p className='title'>{props.title}</p>
+         <div className='price'>
+            <p>${props.price}</p>
+            <small>/ person</small>
+         </div>
       </div>
-      <p className="title">Life lessons with Katie Zafares</p>
-      <div className="price">
-         <p>From $136</p>
-         <small>/ person</small>
-      </div>
-   </div>
-  )
+   )
 }
 
 export default Card
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,11 +1,17 @@
 import '../components/Cards.scss'
 import Card from '../components/Card'
+import data from '../data/data.js'
 
 const Cards = () => {
-  return (
-      <div className="cards section">
-         <Card />
-      </div>
-  )
+   const cardData = data.map((card) => 
+      <Card
+      key={card.id}
+     {...card}
+    />
+   )
+
+   return <div className='cards section'>
+      {cardData}
+   </div>
 }
 export default Cards
